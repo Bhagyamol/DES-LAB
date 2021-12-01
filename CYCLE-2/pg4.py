f79e0035f51c096228742c408382341de592b3bb
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2021-12-01 12:28:55 +0530

    Add files via upload

---
+++ CYCLE-2/pg4.py
@@ -0,0 +1,13 @@
+import numpy as np
+a = np.arange(1,11,1)
+print("Array Elements:")
+print(a)
+first_element= a[:4]
+print("First 4 Elements:")
+print(first_element)
+second_element= a[4:]
+print("Last 6 Elements:")
+print(second_element)
+Third_element=a[1:7]
+print("Elements from index 2 to 7:")
+print(Third_element)
---
+++ CYCLE-2/pg5.py
@@ -0,0 +1,6 @@
+import numpy as np
+array_1d=np.array([0,2,4,6,8,10,12,14,16,18,20,22,24,26,28])
+print("Elements from index 2 to 8 with step 2",array_1d[2:8:2])
+print("Last 3 elements of the array using negative index",array_1d[-3:-1])
+print("Alternate elements of the array",array_1d[::2])
+print("Last 3 alternate elements",array_1d[-3:-1:2])

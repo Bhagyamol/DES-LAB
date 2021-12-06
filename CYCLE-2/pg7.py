1c8f0d321b9d1341a1e0ef988645a9f322b9d1ca
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2021-12-06 11:15:24 +0530

    Add files via upload

---
+++ CYCLE-2/pg7.py
@@ -0,0 +1,36 @@
+import numpy as np
+M1 = np.array([[2, 5], [15, 22]])
+M2 = np.array([[3, 20], [12, 23]])
+M3 = M1 + M2
+print("Matrix addition")
+print(M3)
+M1 = np.array([[2, 5], [15, 22]])
+M2 = np.array([[3, 20], [12, 23]])
+
+M3 = M1 - M2
+print("Matrix Substract")
+print(M3)
+M1 = np.array([[2, 5], [15, 22]])
+M2 = np.array([[3, 20], [12, 23]])
+M3 = M1 / M2
+print("Divide the elements of the matrices")
+print(M3)
+
+M1 = np.array([[2, 6], [5, -10]])
+M2 = np.array([[3, -18], [11, 22]])
+M3 = M1 * M2
+print("Multiply the individual elements of matrix")
+print(M3)
+
+M1 = np.array([[3, 6], [5, -10]])
+M2 = np.array([[9, -18], [11, 22]])
+M3 = M1.dot(M2)
+print("matrix multiplication")
+print(M3)
+M1 = np.array([[3, 6, 9], [5, -10, 15], [4,8,12]])
+M2 = M1.transpose()
+print("Transpose of the matrix")
+print(M2)
+M1 = np.array([[3, 6, 9], [5, -10, 15], [4,8,12]])
+print("Sum of diagonal elements of a matrix")
+print(np.trace(M1))

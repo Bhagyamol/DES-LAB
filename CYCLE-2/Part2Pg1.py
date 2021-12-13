ff6b1f3012024a9fa28c1becebc58b59b31559b1
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2021-12-13 10:49:05 +0530

    Add files via upload

---
+++ CYCLE-2/Part2Pg1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import numpy as nf
+from numpy.linalg import eig
+mat = np.random.randint(10, size=(3, 3))
+array = nf.random.randint(10, size=(3, 3))
+print(mat)
+	
+M_inverse = np.linalg.inv(mat)
+print("Inverse of the array")
+print(M_inverse)
+	
+rank = np.linalg.matrix_rank(mat)
+print("Rank of the given Matrix ")
+print(rank)
+	
+det= np.linalg.det(mat)
+print("Determinant of the given Matrix ")
+print(det)
+
+arr=mat.flatten()
+print("Transform matrix to array ")
+print(arr)
+	
+w,v=eig(array)
+print("Eigen values and vectors")
+print('E-value:', w)
+print('E-vector', v)
+
+
